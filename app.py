55e309cbb861168c1127342fd084a551ef02239b
Anando Ghose <anandoghose@gmail.com>
2023-06-29 22:18:03 +0530

    added login

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import track
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'  # SQLite database file
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')  # SQLite database file
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # Set a secret key for session management
 db = SQLAlchemy(app)
 
 class Task(db.Model):
@@ -15,8 +17,36 @@ class Task(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/tasks', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Call the get_time_entries function with the provided email and password
+        time_entries = track.get_time_entries(email, password)
+        
+        # Check if the result is None, indicating incorrect username or password
+        if time_entries is None:
+            error_message = 'Incorrect username or password.'
+            return render_template('login.html', error_message=error_message)
+        
+        # Store the user's email and password in session
+        session['email'] = email
+        session['password'] = password
+        return redirect('/')
+    
+    return render_template('login.html')
+
+@app.route('/', methods=['GET', 'POST'])
 def tasks():
+    if 'email' not in session or 'password' not in session:
+        return redirect('/login')
+    
+    # Retrieve the user's email and password from session
+    email = session['email']
+    password = session['password']
+
     if request.method == 'POST':
         task_name = request.form['task_name']
         task_duration = request.form['task_duration']
@@ -25,7 +55,7 @@ def tasks():
         if not task_name or not task_duration:
             error_message = 'Please fill in all fields.'
             tasks = Task.query.all()
-            return render_template('tasks.html', tasks=tasks, error_message=error_message)
+            return render_template('index.html', tasks=tasks, error_message=error_message)
 
         # Convert duration to an integer
         try:
@@ -33,18 +63,17 @@ def tasks():
         except ValueError:
             error_message = 'Invalid duration. Please enter a valid number.'
             tasks = Task.query.all()
-            return render_template('tasks.html', tasks=tasks, error_message=error_message)
+            return render_template('index.html', tasks=tasks, error_message=error_message)
 
         task = Task.query.filter_by(name=task_name).first()
         if not task:
             task = Task(name=task_name, duration=task_duration)
             db.session.add(task)
             db.session.commit()
-        return redirect('/tasks')
+        return redirect('/')
 
     # Get tasks from Track
-    all_time_entries = track.get_time_entries()
-    # print(all_tasks)
+    all_time_entries = track.get_time_entries(email, password)
     tasks = Task.query.all()
     descriptions = {}
     for task in tasks:
@@ -58,7 +87,6 @@ def tasks():
             if descriptions[entry['description']] <= 0:
                 completed_tasks += 1
                 descriptions[entry['description']] = 0
-    
 
     total_tasks = len(descriptions)
 
@@ -67,22 +95,21 @@ def tasks():
     else:
         status = f'{completed_tasks}/{total_tasks}'
 
+    return render_template('index.html', tasks=tasks, status=status)
 
-    return render_template('tasks.html', tasks=tasks, status=status)
-
-@app.route('/tasks/delete/<int:task_id>', methods=['POST'])
+@app.route('/delete/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
     task = Task.query.get(task_id)
     if task:
         db.session.delete(task)
         db.session.commit()
-    return redirect('/tasks')
+    return redirect('/')
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/logout', methods=['POST'])
+def logout():
+    # Clear the session data to log out the user
+    session.clear()
+    return redirect('/login')
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(debug=True)
--- track.py
+++ track.py
@@ -4,20 +4,15 @@ from requests.auth import HTTPBasicAuth
 from base64 import b64encode
 from datetime import datetime, timedelta
 
-def get_time_entries():
+def get_time_entries(email, password):
     today = datetime.now()
     tomorrow = today + timedelta(days=1)
-
 
     date_today = today.strftime('%Y-%m-%d')
     date_tomorrow = tomorrow.strftime('%Y-%m-%d')
 
-    # Prepare the payload with query parameters
     payload = {'start_date': date_today, 'end_date': date_tomorrow}
 
-
-    email = os.getenv('TOGGL_EMAIL')  # replace with your email
-    password = os.getenv('TOGGL_PASSWORD')  # replace with your password
     URL = 'https://api.track.toggl.com/api/v9/me/time_entries'
     credentials = b64encode(f"{email}:{password}".encode("utf-8")).decode("ascii")
 
@@ -26,6 +21,10 @@ def get_time_entries():
         'Authorization': f'Basic {credentials}'
     }
 
-    data = requests.get(URL, headers=headers, params=payload, timeout=5)
-    my_data = data.json()
-    return my_data
+    response = requests.get(URL, headers=headers, params=payload, timeout=5)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
